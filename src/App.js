06d1da2b47b6cf7d3bd68e6675001d7aa4a0527b
giorgideveloper <gorgiladze888@gmail.com>
2022-10-06 15:25:31 +0400

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import HoverRating from './Rating';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>holaa</h1>
+			<Button variant='contained'>
+				Hello World <ArrowCircleUpIcon />
+			</Button>
+			<HoverRating />
+		</div>
+	);
 }
 
 export default App;
